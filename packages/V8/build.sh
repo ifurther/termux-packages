cc9d43276b54db08f7361a6b1eb788810b682aa1
ifurther <55025025+ifurther@users.noreply.github.com>
2020-07-10 15:02:29 +0800

    update some part

--- packages/V8/build.sh
+++ packages/V8/build.sh
@@ -8,27 +8,87 @@ TERMUX_PKG_VERSION=8.5.197
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_extract_package() {
-	termux_setup_depot_tools
 	# Provide 'python' as Python v2.
-	mkdir fakebin
+	if [ ! -e fakebin ];then mkdir fakebin; fi
 	(set -e; cd fakebin && ln -sf "$(command -v python2)" python)
 	PATH="$(realpath ./fakebin):$PATH"
 	export PATH
-	if [ ! -d $TERMUX_PKG_SRCDIR ]; then
 
-		rm -Rf $TERMUX_PKG_SRCDIR
-		mkdir -p $TERMUX_PKG_SRCDIR
-		gclient config --unmanaged https://chromium.googlesource.com/v8/v8.git
-		git clone https://github.com/v8/v8.git \
-			$TERMUX_PKG_SRCDIR
-		cd $TERMUX_PKG_SRCDIR
-		local vserioncommit=$(git rev-parse refs/tags/$TERMUX_PKG_VERSION)
-		gclient sync --revision v8@$vserioncommit
-	fi
+	mkdir -p $TERMUX_PKG_SRCDIR
+	cd $TERMUX_PKG_SRCDIR
+
+	git clone --depth=1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
+	export PATH="$(pwd)/depot_tools:${PATH}"
+
+	fetch v8
+
+	cd v8
+	git checkout $TERMUX_PKG_VERSION
+	cd ../
+
+	echo "target_os = ['android']" >> .gclient
+	gclient sync -D --force --reset
+
 }
 
 termux_step_pre_configure() {
-	termux_setup_depot_tools
+	cd v8
+
+	local DEST_CPU
+	if [ $TERMUX_ARCH = "arm" ]; then
+		DEST_CPU="arm"
+	elif [ $TERMUX_ARCH = "i686" ]; then
+		DEST_CPU="ia32"
+	elif [ $TERMUX_ARCH = "aarch64" ]; then
+		DEST_CPU="arm64"
+	elif [ $TERMUX_ARCH = "x86_64" ]; then
+		DEST_CPU="x64"
+	else
+		termux_error_exit "Unsupported arch '$TERMUX_ARCH'"
+	fi
+
 	#gn gen out/foo --args='is_debug=false target_cpu="x64" v8_target_cpu="arm64" use_goma=true'
-	tools/dev/gm.py arm64.release
+	tools/dev/gm.py $DEST_CPU.release
 }
+
+termux_step_make() {
+	cd v8
+
+	local DEST_CPU
+	if [ $TERMUX_ARCH = "arm" ]; then
+		DEST_CPU="arm"
+	elif [ $TERMUX_ARCH = "i686" ]; then
+		DEST_CPU="ia32"
+	elif [ $TERMUX_ARCH = "aarch64" ]; then
+		DEST_CPU="arm64"
+	elif [ $TERMUX_ARCH = "x86_64" ]; then
+		DEST_CPU="x64"
+	else
+		termux_error_exit "Unsupported arch '$TERMUX_ARCH'"
+	fi
+
+	rm -f ./out/*/args.gn
+	ninja -C out.gn/$DEST_CPU.release d8
+}
+
+termux_step_make_install() {
+	cd v8
+
+	local DEST_CPU
+	if [ $TERMUX_ARCH = "arm" ]; then
+		DEST_CPU="ARM"
+	elif [ $TERMUX_ARCH = "i686" ]; then
+		DEST_CPU="IA32"
+	elif [ $TERMUX_ARCH = "aarch64" ]; then
+		DEST_CPU="ARM64"
+	elif [ $TERMUX_ARCH = "x86_64" ]; then
+		DEST_CPU="X64"
+	else
+		termux_error_exit "Unsupported arch '$TERMUX_ARCH'"
+	fi	
+	
+	chmod +x ./.out.gn/${DEST_CPU}.release/d8
+	cp -r ./.out.gn/${DEST_CPU}.release/ ${TERMUX_PREFIX}/lib/V8
+}
+
+
