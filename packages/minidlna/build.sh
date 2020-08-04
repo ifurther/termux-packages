d9613091f36e2e02e026d77f02240475f06b48d1
ifurther <55025025+ifurther@users.noreply.github.com>
2020-08-05 01:35:10 +0800

    change homepage and srcurl

--- packages/minidlna/build.sh
+++ packages/minidlna/build.sh
@@ -1,8 +1,8 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/azatoth/minidlna
+TERMUX_PKG_HOMEPAGE=https://sourceforge.net/projects/minidlna
 TERMUX_PKG_DESCRIPTION="Minidlna is a DLNA server."
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=1.0.22
-TERMUX_PKG_SRCURL=https://github.com/azatoth/minidlna/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_VERSION=1.2.1
+TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/sourceforge/minidlna/minidlna-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=af3f8a08512f1f14484ad4fb207044592cd0699f3ad1a502222fd5577a598e37
 TERMUX_PKG_DEPENDS="libexif, libflac, libvorbis, libsqlite, libid3tag, ffmpeg, libjpeg-turbo, libogg"
 TERMUX_PKG_BUILD_IN_SRC=true
