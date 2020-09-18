96d4dc43d7fe872f329e5bf0aa5f01f3f08e7019
ifurther <55025025+ifurther@users.noreply.github.com>
2020-09-18 17:12:14 +0800

    New package: libcheck

---
+++ packages/libcheck/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://libcheck.github.io/check
+TERMUX_PKG_DESCRIPTION="Check is a unit testing framework for C."
+TERMUX_PKG_LICENSE="LGPL"
+TERMUX_PKG_VERSION=0.15.2
+TERMUX_PKG_SRCURL=https://github.com/libcheck/check/releases/download/$TERMUX_PKG_VERSION/check-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=a8de4e0bacfb4d76dd1c618ded263523b53b85d92a146d8835eb1a52932fa20a
+#TERMUX_PKG_DEPENDS=""
+#TERMUX_PKG_BUILD_IN_SRC=true
